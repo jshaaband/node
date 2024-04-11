4813cc880509c2768cdb6b59bede854d792d0208
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-04-12 01:48:53 +0300

    add worker process

---
+++ complex/worker/inde.js
@@ -0,0 +1,21 @@
+const key = require("./keys.js");
+const redis = require('redis')
+
+const redisClient = redis.createClient({
+    host: key.redisHost,
+    port: key.redisPort,
+    retry_strategy: ()=>1000
+})
+
+const sub =  redisClient.duplicates();
+
+function fib(index){
+    if(index<2) return 1;
+    return fib(index - 1) + fib(index - 2)
+}
+
+sub.on('message', (channel, message)=>{
+    redisClient.hset('values',message,fib(parseInt(message)));
+})
+
+sub.subscribe('insert')
