21f598dced90f7d5b4516e6550a18c21314f8a3a
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-04-17 14:51:56 +0300

    add server & worker docker files

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import { Route, Link} from "react-router-dom";
 import OtherPage from "./OtherPage";
 import Fib from "./Fib"
 
