9049ec7b15dc156fed623a9a71994a1bb5198069
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-04-19 23:52:23 +0300

    troubleshooting start up bugs

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -1,36 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Link} from "react-router-dom";
-import OtherPage from "./OtherPage";
-import Fib from "./Fib"
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">OtherPage</Link>
-      </header>
-      <div>
-        <Route exact path="/" Component={Fib}></Route>
-        <Route exact path="/othepage" Component={OtherPage}></Route>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
-
-
--- complex/client/src/OtherPage.js
+++ complex/client/src/OtherPage.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const page =  ()=>{
-    return(
-        <div>
-            I'm some other page
-            <Link to="/">Go Back Home</Link>
-        </div>
-    )
-}
-
-export default page
+export default () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
