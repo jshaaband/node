8af0fceea290416282c441054e61073de5b3fd6e
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-04-20 00:56:40 +0300

    bug fixes

--- complex/client/src/OtherPage.js
+++ complex/client/src/OtherPage.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom"
 
-export default () => {
-  return (
-    <div>
-      Im some other page!
-      <Link to="/">Go back home</Link>
-    </div>
-  );
-};
+const page =  ()=>{
+    return(
+        <div>
+            I'm some other page
+            <Link to="/">Go Back Home</Link>
+        </div>
+    )
+}
+
+export default page
