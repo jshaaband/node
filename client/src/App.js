dd0d795d1cf5e4c085ae73da824538cc84140a99
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-05-02 17:45:58 +0300

    enhance ui with prime components

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { PrimeReactProvider } from 'primereact/api';
 import { Button } from 'primereact/button';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+import MenuBar from './MenuBar';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -22,78 +23,14 @@ function App() {
 
         <PrimeReactProvider value={value}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Fib calculator
-          </a>
+          <MenuBar></MenuBar>
+          <div>
+              <Route exact path="/" component={Fib}/>
+              <Route path="/otherpage" component={OtherPage}/>
+            </div>
 
 
-<div className="surface-0 text-center">
-    <div className="mb-3 font-bold text-3xl">
-        <span className="text-900">One Product, </span>
-        <span className="text-blue-600">Many Solutions</span>
-    </div>
-    <div className="text-700 mb-6">Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna.</div>
-    <div className="grid">
-        <div className="col-12 md:col-4 mb-4 px-5">
-            <span className="p-3 shadow-2 mb-3 inline-block" style={{borderRadius: '10px'}}>
-                <i className="pi pi-desktop text-4xl text-blue-500"></i>
-            </span>
-            <div className="text-900 text-xl mb-3 font-medium">Built for Developers</div>
-            <span className="text-700 line-height-3">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</span>
-        </div>
-        <div className="col-12 md:col-4 mb-4 px-5">
-            <span className="p-3 shadow-2 mb-3 inline-block" style={{borderRadius: '10px'}}>
-                <i className="pi pi-lock text-4xl text-blue-500"></i>
-            </span>
-            <div className="text-900 text-xl mb-3 font-medium">End-to-End Encryption</div>
-            <span className="text-700 line-height-3">Risus nec feugiat in fermentum posuere urna nec. Posuere sollicitudin aliquam ultrices sagittis.</span>
-        </div>
-        <div className="col-12 md:col-4 mb-4 px-5">
-            <span className="p-3 shadow-2 mb-3 inline-block" style={{borderRadius: '10px'}}>
-                <i className="pi pi-check-circle text-4xl text-blue-500"></i>
-            </span>
-            <div className="text-900 text-xl mb-3 font-medium">Easy to Use</div>
-            <span className="text-700 line-height-3">Ornare suspendisse sed nisi lacus sed viverra tellus. Neque volutpat ac tincidunt vitae semper.</span>
-        </div>
-        <div className="col-12 md:col-4 mb-4 px-5">
-            <span className="p-3 shadow-2 mb-3 inline-block" style={{borderRadius: '10px'}}>
-                <i className="pi pi-globe text-4xl text-blue-500"></i>
-            </span>
-            <div className="text-900 text-xl mb-3 font-medium">Fast & Global Support</div>
-            <span className="text-700 line-height-3">Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus.</span>
-        </div>
-        <div className="col-12 md:col-4 mb-4 px-5">
-            <span className="p-3 shadow-2 mb-3 inline-block" style={{borderRadius: '10px'}}>
-                <i className="pi pi-github text-4xl text-blue-500"></i>
-            </span>
-            <div className="text-900 text-xl mb-3 font-medium">Open Source</div>
-            <span className="text-700 line-height-3">Nec tincidunt praesent semper feugiat. Sed adipiscing diam donec adipiscing tristique risus nec feugiat. </span>
-        </div>
-        <div className="col-12 md:col-4 md:mb-4 mb-0 px-3">
-            <span className="p-3 shadow-2 mb-3 inline-block" style={{borderRadius: '10px'}}>
-                <i className="pi pi-shield text-4xl text-blue-500"></i>
-            </span>
-            <div className="text-900 text-xl mb-3 font-medium">Trusted Securitty</div>
-            <span className="text-700 line-height-3">Mattis rhoncus urna neque viverra justo nec ultrices. Id cursus metus aliquam eleifend.</span>
-        </div>
-    </div>
-</div>
 
-
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <div>
-          <Route exact path="/" component={Fib}/>
-          <Route path="/otherpage" component={OtherPage}/>
-        </div>
       </div>
           </PrimeReactProvider>
     </Router>
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Button} from "primereact/button";
+import { InputText } from 'primereact/inputtext';
 
 class Fib extends Component {
   state = {
@@ -57,21 +59,31 @@ class Fib extends Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Enter your index:</label>
-          <input
-            value={this.state.index}
-            onChange={(event) => this.setState({ index: event.target.value })}
-          />
-          <button>Submit</button>
+        <div>
+        <div className="surface-0 text-700 text-center pt-8">
+            <div className="text-blue-600 font-bold mb-3"><i className="pi pi-discord"></i>&nbsp;POWERED BY DISCORD</div>
+            <div className="text-900 font-bold text-5xl mb-3">Join Our Design Community</div>
+            <div className="text-700 text-2xl mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos.</div>
+
+        </div>
+
+        <form onSubmit={this.handleSubmit} className={"pt-2"}>
+          <label className={"text-900 font-bold text-5xl m-5"}>Enter your index:</label><br/>
+
+          <div className="card flex justify-content-center my-4">
+            <InputText value={this.state.index} onChange={(e) => this.setState({index: e.target.value})}/>
+        </div>
+          <div className="card flex justify-content-center">
+             <Button label="Submit" icon="pi pi-save"
+                     className="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"/>
+        </div>
         </form>
 
         <h3>Indexes I have seen:</h3>
-        {this.renderSeenIndexes()}
+          {this.renderSeenIndexes()}
 
-        <h3>Calculated Values:</h3>
-        {this.renderValues()}
+          <h3>Calculated Values:</h3>
+          {this.renderValues()}
       </div>
     );
   }
