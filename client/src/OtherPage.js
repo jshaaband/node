1c9a0fa89bb1c55b5bc9c19ee1fe30244c6fe9ea
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-05-12 22:46:19 +0300

    add page header

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "./Layout/Layout";
-import { Button } from "primereact/button";
 
 const OtherPage = () => {
   return (
@@ -8,46 +7,37 @@ const OtherPage = () => {
       <Layout></Layout>
 
       <div className="surface-0">
-        <ul className="list-none p-0 m-0 flex align-items-center font-medium mb-3">
-          <li>
-            <a className="text-500 no-underline line-height-3 cursor-pointer">
-              Application
+        <ul className="list-none py-3 px-5 m-0 surface-section flex align-items-center font-medium overflow-x-auto border-top-1 border-bottom-1 surface-border shadow-2 lg:shadow-none">
+          <li className="pr-3">
+            <a className="cursor-pointer">
+              <i className="pi pi-home text-blue-500"></i>
             </a>
           </li>
           <li className="px-2">
-            <i className="pi pi-angle-right text-500 line-height-3"></i>
+            <i className="pi pi-angle-right text-500 font-medium"></i>
           </li>
-          <li>
-            <span className="text-900 line-height-3">Analytics</span>
+          <li className="px-2">
+            <a className="cursor-pointer text-blue-500 white-space-nowrap font-medium">
+              Level 3
+            </a>
+          </li>
+          <li className="px-2">
+            <i className="pi pi-angle-right text-500"></i>
+          </li>
+          <li className="px-2">
+            <a className="cursor-pointer text-blue-500 white-space-nowrap font-medium">
+              Level 2
+            </a>
+          </li>
+          <li className="px-2">
+            <i className="pi pi-angle-right text-500"></i>
+          </li>
+          <li className="px-2">
+            <a className="cursor-pointer text-900 white-space-nowrap font-medium">
+              Level 1
+            </a>
           </li>
         </ul>
-        <div className="flex align-items-start flex-column lg:justify-content-between lg:flex-row">
-          <div>
-            <div className="font-medium text-3xl text-900">Customers</div>
-            <div className="flex align-items-center text-700 flex-wrap">
-              <div className="mr-5 flex align-items-center mt-3">
-                <i className="pi pi-users mr-2"></i>
-                <span>332 Active Users</span>
-              </div>
-              <div className="mr-5 flex align-items-center mt-3">
-                <i className="pi pi-globe mr-2"></i>
-                <span>9402 Sessions</span>
-              </div>
-              <div className="flex align-items-center mt-3">
-                <i className="pi pi-clock mr-2"></i>
-                <span>2.32m Avg. Duration</span>
-              </div>
-            </div>
-          </div>
-          <div className="mt-3 lg:mt-0">
-            <Button
-              label="Add"
-              className="p-button-outlined mr-2"
-              icon="pi pi-user-plus"
-            />
-            <Button label="Save" icon="pi pi-check" />
-          </div>
-        </div>
       </div>
 
       <div className="surface-0 pt-8 text-center mx-6">
