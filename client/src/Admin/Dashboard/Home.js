eab3a75b1f2c4e436dc8555a4175bf94cb634846
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-05-27 22:56:13 +0300

    enhance dashboard ui

--- client/src/Admin/Dashboard/Home.js
+++ client/src/Admin/Dashboard/Home.js
@@ -217,6 +217,17 @@ const HomePage = () => {
               </div>
             </div>
           </div>
+
+          <div className="col-12  surface-0 py-4 shadow-2 border-round">
+            <div className="text-3xl font-medium text-900 mb-3">Card Title</div>
+            <div className="font-medium text-500 mb-3">
+              Vivamus id nisl interdum, blandit augue sit amet, eleifend mi.
+            </div>
+            <div
+              style={{ height: "150px" }}
+              className="border-2 border-dashed border-300"
+            ></div>
+          </div>
         </div>
       </div>
     </div>
