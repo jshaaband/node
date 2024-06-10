dc0ea4e1b8519e6f56c81ca77bf3c41ececc3054
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-06-11 00:05:05 +0300

    ui enhancements

--- client/src/Admin/Projects/UIkit.js
+++ client/src/Admin/Projects/UIkit.js
@@ -12,30 +12,20 @@ import illustration3 from "../../images/img_10.png";
 import chart1 from "../../images/img_11.png";
 import chart2 from "../../images/img_12.png";
 import FooterPage from "../../Layout/Footer/Footer";
+import Layout from "../../Layout/Layout";
 
 const UIKitPage = () => {
   return (
-    <div>
+    <>
+      <Layout></Layout>
       <div className="block-content">
         <div>
           <div className="min-h-screen flex flex-column">
-            <div
-              className="bg-indigo-500 py-3 px-5 flex align-items-center justify-content-between relative lg:static"
-              style={{ height: "80px" }}
-            >
-              <img
-                src={hyper}
-                alt="hyper-300"
-                style={{ height: "40px" }}
-                className="mr-0 lg:mr-6"
-              />
+            <div className="bg-indigo-500 px-5 flex align-items-center justify-content-between relative lg:static">
+              <img src={hyper} alt="hyper-300" className="mr-0 lg:mr-6" />
               <a className="p-ripple cursor-pointer block lg:hidden text-indigo-50">
                 <i className="pi pi-bars text-4xl"></i>
-                <span
-                  role="presentation"
-                  className="p-ink"
-                  style={{ height: "0px", width: "0px" }}
-                ></span>
+                <span role="presentation" className="p-ink"></span>
               </a>
               <div className="align-items-center flex-grow-1 justify-content-between hidden lg:flex absolute lg:static w-full bg-indigo-500 left-0 top-100 z-1 shadow-2 lg:shadow-none">
                 <ul className="list-none p-0 m-0 flex lg:align-items-center select-none flex-column lg:flex-row">
@@ -43,11 +33,7 @@ const UIKitPage = () => {
                     <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
                       <i className="pi pi-home mr-2"></i>
                       <span>Home</span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "100px", width: "100px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </a>
                   </li>
                   <li>
@@ -55,27 +41,14 @@ const UIKitPage = () => {
                       <i className="pi pi-users mr-2"></i>
                       <span>Customers</span>
                       <i className="pi pi-angle-down ml-auto lg:ml-3"></i>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{
-                          height: "168.836px",
-                          width: "168.836px",
-                          top: "-69px",
-                          left: "35.9062px",
-                        }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </a>
                     <ul className="list-none py-3 px-6 m-0 lg:px-0 lg:py-0 border-round shadow-0 lg:border-1 border-50 lg:shadow-2 lg:absolute surface-overlay origin-top w-full lg:w-15rem cursor-pointer hidden">
                       <li>
                         <a className="p-ripple flex p-3 align-items-center text-600 hover:text-900 hover:surface-100 transition-colors transition-duration-150 w-full">
                           <i className="pi pi-user-plus mr-2"></i>
                           <span className="font-medium">Add New</span>
-                          <span
-                            role="presentation"
-                            className="p-ink"
-                            style={{ height: "0px", width: "0px" }}
-                          ></span>
+                          <span role="presentation" className="p-ink"></span>
                         </a>
                       </li>
                       <li className="relative">
@@ -83,16 +56,7 @@ const UIKitPage = () => {
                           <i className="pi pi-search mr-2"></i>
                           <span className="font-medium">Search</span>
                           <i className="pi pi-angle-down ml-auto lg:-rotate-90"></i>
-                          <span
-                            role="presentation"
-                            className="p-ink"
-                            style={{
-                              height: "238px",
-                              width: "238px",
-                              top: "27.5px",
-                              left: "158.406px",
-                            }}
-                          ></span>
+                          <span role="presentation" className="p-ink"></span>
                         </a>
                         <ul className="list-none py-3 pl-3 m-0 lg:px-0 lg:py-0 border-round shadow-0 lg:border-1 border-50 lg:shadow-2 lg:absolute surface-overlay origin-top w-full lg:w-15rem cursor-pointer left-100 top-0 hidden">
                           <li>
@@ -102,7 +66,6 @@ const UIKitPage = () => {
                               <span
                                 role="presentation"
                                 className="p-ink"
-                                style={{ height: "0px", width: "0px" }}
                               ></span>
                             </a>
                           </li>
@@ -113,7 +76,6 @@ const UIKitPage = () => {
                               <span
                                 role="presentation"
                                 className="p-ink"
-                                style={{ height: "0px", width: "0px" }}
                               ></span>
                             </a>
                           </li>
@@ -125,27 +87,14 @@ const UIKitPage = () => {
                     <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
                       <i className="pi pi-calendar mr-2"></i>
                       <span>Calendar</span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "122.695px", width: "122.695px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </a>
                   </li>
                   <li>
                     <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
                       <i className="pi pi-chart-line mr-2"></i>
                       <span>Stats</span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{
-                          height: "94.5px",
-                          width: "94.5px",
-                          top: "-21px",
-                          left: "-44.625px",
-                        }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </a>
                   </li>
                 </ul>
@@ -154,11 +103,7 @@ const UIKitPage = () => {
                     <a className="p-ripple flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-indigo-100 hover:text-indigo-50 hover:bg-indigo-600 font-medium border-round cursor-pointer transition-colors transition-duration-150 w-full">
                       <i className="pi pi-inbox text-base lg:text-2xl mr-2 lg:mr-0"></i>
                       <span className="block lg:hidden font-medium">Inbox</span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "56px", width: "56px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </a>
                   </li>
                   <li>
@@ -167,11 +112,7 @@ const UIKitPage = () => {
                       <span className="block lg:hidden font-medium">
                         Notifications
                       </span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "56px", width: "56px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </a>
                   </li>
                   <li className="border-top-1 border-indigo-400 lg:border-top-none">
@@ -180,7 +121,6 @@ const UIKitPage = () => {
                         src={avatar}
                         alt="avatar-f-1"
                         className="mr-3 lg:mr-0"
-                        style={{ width: "32px", height: "32px" }}
                       />
                       <div className="block lg:hidden">
                         <div className="text-indigo-50 font-medium">
@@ -190,11 +130,7 @@ const UIKitPage = () => {
                           Marketing Specialist
                         </span>
                       </div>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "64px", width: "64px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </a>
                   </li>
                 </ul>
@@ -231,11 +167,11 @@ const UIKitPage = () => {
                 </a>
               </li>
             </ul>
-            <div className="p-5 flex flex-column flex-auto">
+            <div className=" flex flex-column flex-auto">
               <div className="grid">
                 <div className="col-12">
-                  <div className="surface-card shadow-2 border-round flex p-3 flex-column md:flex-row">
-                    <div className="border-bottom-1 md:border-right-1 md:border-bottom-none surface-border flex-auto p-3">
+                  <div className="surface-card flex p-3 flex-column md:flex-row">
+                    <div className="border-bottom-1 md:border-right-1 md:border-bottom-none shadow-3 mr-4 surface-border flex-auto p-3">
                       <div className="flex align-items-center mb-3">
                         <i className="pi pi-shopping-cart text-blue-500 text-xl mr-2"></i>
                         <span className="text-500 font-medium">Orders</span>
@@ -248,7 +184,7 @@ const UIKitPage = () => {
                         <span className="text-pink-500 font-medium ">-25</span>
                       </div>
                     </div>
-                    <div className="border-bottom-1 md:border-right-1 md:border-bottom-none surface-border flex-auto p-3">
+                    <div className="border-bottom-1 md:border-right-1 md:border-bottom-none shadow-3 mr-4 surface-border flex-auto p-3">
                       <div className="flex align-items-center mb-3">
                         <i className="pi pi-shopping-cart text-orange-500 text-xl mr-2"></i>
                         <span className="text-500 font-medium">Revenue</span>
@@ -261,7 +197,7 @@ const UIKitPage = () => {
                         <span className="text-green-500 font-medium ">+15</span>
                       </div>
                     </div>
-                    <div className="border-bottom-1 md:border-right-1 md:border-bottom-none surface-border flex-auto p-3">
+                    <div className="border-bottom-1 md:border-right-1 md:border-bottom-none shadow-3 mr-4 surface-border flex-auto p-3">
                       <div className="flex align-items-center mb-3">
                         <i className="pi pi-users text-cyan-500 text-xl mr-2"></i>
                         <span className="text-500 font-medium">Customers</span>
@@ -276,7 +212,7 @@ const UIKitPage = () => {
                         </span>
                       </div>
                     </div>
-                    <div className="flex-auto p-3">
+                    <div className="flex-auto p-3 shadow-3 mr-4">
                       <div className="flex align-items-center mb-3">
                         <i className="pi pi-users text-purple-500 text-xl mr-2"></i>
                         <span className="text-500 font-medium">Comments</span>
@@ -301,11 +237,7 @@ const UIKitPage = () => {
                         <button className="p-button p-component p-button-text p-button-plain p-button-rounded p-button-icon-only">
                           <span className="p-button-icon p-c pi pi-ellipsis-v"></span>
                           <span className="p-button-label p-c">&nbsp;</span>
-                          <span
-                            role="presentation"
-                            className="p-ink"
-                            style={{ height: "48px", width: "48px" }}
-                          ></span>
+                          <span role="presentation" className="p-ink"></span>
                         </button>
                       </div>
                     </div>
@@ -442,11 +374,7 @@ const UIKitPage = () => {
                         <button className="p-button p-component p-button-text p-button-plain p-button-rounded p-button-icon-only">
                           <span className="p-button-icon p-c pi pi-ellipsis-v"></span>
                           <span className="p-button-label p-c">&nbsp;</span>
-                          <span
-                            role="presentation"
-                            className="p-ink"
-                            style={{ height: "48px", width: "48px" }}
-                          ></span>
+                          <span role="presentation" className="p-ink"></span>
                         </button>
                       </div>
                     </div>
@@ -544,11 +472,7 @@ const UIKitPage = () => {
                     >
                       <span className="p-button-icon p-c p-button-icon-left pi pi-arrow-right p-button-rounded"></span>
                       <span className="p-button-label p-c">Learn More</span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "153.062px", width: "153.062px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </button>
                   </div>
                 </div>
@@ -572,11 +496,7 @@ const UIKitPage = () => {
                     >
                       <span className="p-button-icon p-c p-button-icon-left pi pi-arrow-right p-button-rounded"></span>
                       <span className="p-button-label p-c">Learn More</span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "153.062px", width: "153.062px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </button>
                   </div>
                 </div>
@@ -600,11 +520,7 @@ const UIKitPage = () => {
                     >
                       <span className="p-button-icon p-c p-button-icon-left pi pi-arrow-right p-button-rounded"></span>
                       <span className="p-button-label p-c">Learn More</span>
-                      <span
-                        role="presentation"
-                        className="p-ink"
-                        style={{ height: "153.062px", width: "153.062px" }}
-                      ></span>
+                      <span role="presentation" className="p-ink"></span>
                     </button>
                   </div>
                 </div>
@@ -616,11 +532,7 @@ const UIKitPage = () => {
                         <button className="p-button p-component p-button-text p-button-plain p-button-rounded p-button-icon-only">
                           <span className="p-button-icon p-c pi pi-ellipsis-v"></span>
                           <span className="p-button-label p-c">&nbsp;</span>
-                          <span
-                            role="presentation"
-                            className="p-ink"
-                            style={{ height: "48px", width: "48px" }}
-                          ></span>
+                          <span role="presentation" className="p-ink"></span>
                         </button>
                       </div>
                     </div>
@@ -637,11 +549,7 @@ const UIKitPage = () => {
                         <button className="p-button p-component p-button-text p-button-plain p-button-rounded p-button-icon-only">
                           <span className="p-button-icon p-c pi pi-ellipsis-v"></span>
                           <span className="p-button-label p-c">&nbsp;</span>
-                          <span
-                            role="presentation"
-                            className="p-ink"
-                            style={{ height: "48px", width: "48px" }}
-                          ></span>
+                          <span role="presentation" className="p-ink"></span>
                         </button>
                       </div>
                     </div>
@@ -654,7 +562,7 @@ const UIKitPage = () => {
         </div>
       </div>
       <FooterPage></FooterPage>
-    </div>
+    </>
   );
 };
 
