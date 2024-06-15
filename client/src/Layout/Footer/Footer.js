604c1178f4fcee498631203f76cabfd2e97ff8db
JOHNSON DIDINYA <didinkaj@gmail.com>
2024-06-15 22:27:55 +0300

    enhance footer ui

--- client/src/Layout/Footer/Footer.js
+++ client/src/Layout/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "primereact/button";
+import footer from "../../images/img_17.png";
 
 const FooterPage = () => {
   return (
@@ -21,6 +22,39 @@ const FooterPage = () => {
           className="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"
         />
       </div>
+      <div className="surface-section py-6 px-6 border-top-1 surface-border">
+        <div className="flex flex flex-column lg:flex-row lg:align-items-center justify-content-between">
+          <div className="flex align-items-center">
+            <img
+              src={footer}
+              alt="Image"
+              height="40"
+              className="mr-0 lg:mr-6"
+            />
+          </div>
+          <div className="flex flex-column lg:flex-row lg:align-items-center lg:my-0 my-4">
+            <a className="text-700 cursor-pointer mb-2 lg:mb-0">Brand Policy</a>
+            <a className="text-700 cursor-pointer mb-2 lg:mb-0 ml-0 lg:ml-4">
+              Privacy Policy
+            </a>
+            <a className="text-700 cursor-pointer mb-2 lg:mb-0 ml-0 lg:ml-4">
+              Terms of Service
+            </a>
+          </div>
+          <div className="flex align-items-center lg:justify-content-between">
+            <a className="cursor-pointer text-500">
+              <i className="pi pi-twitter text-xl"></i>
+            </a>
+            <a className="cursor-pointer text-500 ml-2">
+              <i className="pi pi-facebook text-xl"></i>
+            </a>
+            <a className="cursor-pointer text-500 ml-2">
+              <i className="pi pi-github text-xl"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
       <div className="bg-bluegray-900 text-gray-100 p-3 flex justify-content-between lg:justify-content-center align-items-center flex-wrap">
         <div className="font-bold mr-8">🔥 Hot Deals!</div>
         <div className="align-items-center hidden lg:flex">
